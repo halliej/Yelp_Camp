5a594fc21196053e8b2d7e53922716c0bcc71cd4
Hal DeLage <hal.delage@teamaol.com>
2017-12-06 16:29:35 -0500

    Added update comments functionality

--- routes/comments.js
+++ routes/comments.js
@@ -38,6 +38,26 @@ router.post('/', isLoggedIn, (req, res) => {
   });
 });
 
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, comment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', { campground_id: req.params.id, comment });
+    }
+  });
+});
+
+router.put('/:comment_id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
