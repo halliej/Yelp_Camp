1614e01a6bebe47793029d927d32f81ad7ee0bb7
Hal DeLage <hal.delage@teamaol.com>
2017-12-07 16:44:57 -0500

    Started adding delete comments functionality

--- routes/comments.js
+++ routes/comments.js
@@ -58,6 +58,16 @@ router.put('/:comment_id', (req, res) => {
   });
 });
 
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`)
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
